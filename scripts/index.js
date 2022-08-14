f8e3382e595ff1d2803a3039bfa102f28b84e647
andrew <titarenkoandrew@yandex.ru>
2022-08-14 19:19:52 +0300

    fix: исправить ошибки после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,25 @@
 const popup = document.querySelector('.popup');
-const openButton = document.querySelector('.popup__open');
+const openButton = document.querySelector('.profile__button-edit-name-opened');
 const closeButton = popup.querySelector('.popup__close-button');
 const saveButton = popup.querySelector('.popup__save-button');
 
 let formElement = document.querySelector('.popup__form'); 
-let nameInput = formElement.querySelector('.popup__input-name');
-let jobInput = formElement.querySelector('.popup__input-description');
+let nameInput = formElement.querySelector('.popup__input_type_name');
+let jobInput = formElement.querySelector('.popup__input_type_description');
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
 
+function popupValue() {
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileDescription.textContent;
+}
 
 let togglePopup = function () {
-    popup.classList.toggle('popup__opened');
+    popup.classList.toggle('popup_action_open');
 }
 
 openButton.addEventListener('click', togglePopup);
 closeButton.addEventListener('click', togglePopup);
-saveButton.addEventListener('click', togglePopup);
 
 
 function formSubmitHandler(evt) {
@@ -24,6 +27,9 @@ function formSubmitHandler(evt) {
 
     profileName.textContent = nameInput.value;
     profileDescription.textContent = jobInput.value;
+    
+    saveButton.addEventListener('click', togglePopup);
+
 
 }
 
